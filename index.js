88e8f7e4e2ac8b4b8723e80d49cf4c92be0c2a34
Mayankkatheriya <mayankkatheriya4@gmail.com>
2025-02-13 22:11:48 +0530

    first APi added

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send the response body
+  res.end("Hello, World!\n");
+});
+
+server.listen(5000, () => {
+  console.log("Server listening on port 3000");
+});
