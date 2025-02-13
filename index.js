e19422bb8539fe313a2ea8692ef469d2fde2a4de
Mayankkatheriya <mayankkatheriya4@gmail.com>
2025-02-13 22:17:46 +0530

    added path

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = http.createServer((req, res) => {
   // Set the response header
   res.writeHead(200, { "Content-Type": "text/plain" });
 
+  if(req.url === "/product") {
+    res.end("Product Page");
+  }
+
   // Send the response body
   res.end("Hello, World!\n");
 });
